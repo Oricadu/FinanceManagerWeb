85d75ed453623ab6128600fb651d38be388404c6
Oricadu <maksimova0130@gmail.com>
2022-11-13 21:55:21 +0400

    Initial commit

---
+++ src/main/java/com/oricadu/financemanagerweb/FinanceManagerWebApplication.java
@@ -0,0 +1,13 @@
+package com.oricadu.financemanagerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceManagerWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceManagerWebApplication.class, args);
+    }
+
+}
